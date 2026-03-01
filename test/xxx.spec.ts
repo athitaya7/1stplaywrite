43069807a4204d95cba46a6aefb06f26196d7af0
athitaya7 <143474219+athitaya7@users.noreply.github.com>
2026-03-01 10:35:28 +0000

    Assignment Playwright#1

---
+++ test/xxx.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://demo.playwright.dev/todomvc/#/');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).click();
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('Read a book');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('meeting');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('Play with my fatty cat');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
+  await page.getByRole('link', { name: 'Completed' }).click();
+});
